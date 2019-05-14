ad8af1f3b5d48145ce1c81817637b2ffa637090c
Chelsia Liu <chelsia.liu3@gmail.com>
2019-05-14 15:15:49 -0600

    Adding in faker to generate fake data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@
 // Import the React and ReactDOM libraries
 import React from 'react'; //find folder in node_modules
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 
 // Create a React component (function-based component)
 // Use babeljs.io/ to see how JSX gets compiled
@@ -17,7 +18,7 @@ const App = () => { // () => { replaces function() {
 		//use className instead of class to avoid collisions in declaring
 		//another Javascript class
 		//use htmlFor instead of for to avoid confusion with JS for loop
-		<div>
+/*		<div>
 		  <label className="label" htmlFor="name">
 		  	{labelText}
 		  </label>
@@ -25,6 +26,24 @@ const App = () => { // () => { replaces function() {
 		  <button style={style}>
 		  	{buttonText.text}
 		  </button>
+		</div>*/
+
+		// Comments container
+		<div className="ui container comments">
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.avatar()}/>
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+				</div>
+				<div className="metadata">
+					<span className="date">Today at 6:00 PM</span>
+				</div>
+				<div className="text">Nice blog post!</div>
+			</div>
 		</div>
 	);
 };
